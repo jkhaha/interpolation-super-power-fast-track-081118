7bff474db8b64c5f08dd53a40ea8816a9b9b1ea4
Jackie Ha <jha2@wellesley.edu>
2018-08-11 16:46:39 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,5 +5,5 @@ def display_rainbow(color_array)
   
   # want this to be puts'ed: 
   # "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
-  puts "R: #{color_array[0]}, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  puts "R: #{color_array[0]}, O: #{color_array[1]}, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 display_rainbow (color_array)
