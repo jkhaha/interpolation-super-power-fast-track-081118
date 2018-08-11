14d34a2b70d4d5e03b60b72743f7ffae09b9d2f6
Jackie Ha <jha2@wellesley.edu>
2018-08-11 16:24:50 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 # display_rainbow
 def display_rainbow 
-  display_ranbow (['red', 'orange', 'yellow', 'green])
+  display_ranbow (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 end
